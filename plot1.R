aa7a8431df5d186405b75e7756dda0585616b331
jmtzr7 <jmtzr7@users.noreply.github.com>
2015-09-13 14:46:55 -0500

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,1 +1,21 @@
-
+plot1 = function(){
+  ## Read in data
+  data=read.table("household_power_consumption.txt", header=TRUE, sep=";")
+  
+  ## Covert Date field to date/time type
+  data$Date=as.Date(data$Date, format="%d/%m/%Y")
+  
+  ## create subset of data
+  SubData=data[(data$Date=="2007-02-01") | (data$Date=="2007-02-02"),]
+  
+  ## Convert data from text to numeric
+  SubData$Global_active_power = as.numeric(as.character(SubData$Global_active_power))
+  
+  ## Create plot
+  par(mfrow=c(1,1))
+  hist(SubData$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", col = "red")
+  
+  ## Copy graph to png file
+  dev.copy(png, file="plot1.png", width=480, height=480)
+  dev.off()
+}
